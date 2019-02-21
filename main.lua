f70fc04148aae426eb563f10e80193b50dc221e9
avani-1660 <47641293+avani-1660@users.noreply.github.com>
2019-02-21 12:18:48 -0500

    Add files via upload

---
+++ main.lua
@@ -0,0 +1,19 @@
+-----------------------------------------------------------------------------------------
+--
+-- main.lua
+--
+-----------------------------------------------------------------------------------------
+
+ local soccerBall = display.newImageRect( "images.jfif", 500, 480 )
+soccerBall.x = display.contentCenterX
+soccerBall.y = display.contentCenterY
+soccerBall.id = "ball object"
+ 
+local function onObjectTouch( event )
+    -- it is not perfect, but we will make it better soon
+    print( "Touched ball ")
+
+    return true
+end
+
+soccerBall:addEventListener( "touch", onObjectTouch )
